b154116ac6f76989ab9b9c916f94abe539f713d3
kcanaytekin <kemal.aytekin@bilyoner.com>
2024-01-23 01:13:14 +0300

    DriverFactory is added

---
+++ src/main/java/Util/DriverFactory.java
@@ -0,0 +1,46 @@
+package Util;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class DriverFactory {
+    static WebDriver driver;
+    static Properties properties;
+    public static WebDriver initializeDriver(String browser){
+        properties = ConfigReader.getProperties();
+        if (browser.equals("Chrome")){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+
+        else if (browser.equals("Firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+
+        else {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
+
+        String url = properties.getProperty("url");
+        int pageLoadTimeout = Integer.parseInt(properties.getProperty("pageLoadTimeout"));
+        int impWait = Integer.parseInt(properties.getProperty("impWait"));
+        driver.get(url);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(pageLoadTimeout, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(impWait, TimeUnit.SECONDS);
+
+        return getDriver();
+    }
+
+    public static WebDriver getDriver(){
+        return driver;
+    }
+}
