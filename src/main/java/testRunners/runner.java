50e0611bb15290d45fc2fa50a94a5997c7bbd8e1
kcanaytekin <kemal.aytekin@bilyoner.com>
2024-01-30 01:37:47 +0300

    ElementHelper, runner and xml are added

---
+++ src/main/java/testRunners/runner.java
@@ -0,0 +1,19 @@
+package testRunners;
+
+import Util.DriverFactory;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.openqa.selenium.WebDriver;
+
+@CucumberOptions(
+        features = {"src/test/java/Features"},
+        glue = {"StepDefinitions"},
+        tags = "",
+        plugin = {
+                "summary","pretty","html:Reports/CucumberReport/reports.html"
+        }
+)
+
+public class runner extends AbstractTestNGCucumberTests {
+    static WebDriver driver = DriverFactory.getDriver()
+}
